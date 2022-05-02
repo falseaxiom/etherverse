77f0bccb1a52709eaa1a69d40fb9b9713026adda
Joanna Kuo <jkuo@princeton.edu>
2022-05-02 01:37:02 -0400

    add test

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -119,6 +119,7 @@ user.onload();
 
 // create contract
 let contract = new Contract();
+contract.loadContract();
 
 // Land information
 addLand();
--- frontend/src/components/blockchain/contract.js
+++ frontend/src/components/blockchain/contract.js
@@ -1,18 +1,154 @@
+const Web3 = require("web3");
+
 class Contract {
   constructor() {
     this.state = {
-      contract: [],
+      contracts: {},
+      todoList: [],
     };
   }
 
   async loadContract() {
-    // Create a JavaScript version of the smart contract
-    const todoList = await $.getJSON("TodoList.json");
-    App.contracts.TodoList = TruffleContract(todoList);
-    App.contracts.TodoList.setProvider(App.web3Provider);
+    const web3 = new Web3("http://localhost:7545");
 
-    // Hydrate the smart contract with values from the blockchain
-    App.todoList = await App.contracts.TodoList.deployed();
+    const address = "0xe3649f94733D11e163508408bfae14286C7bDb70";
+    const abi = JSON.parse(`[
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "tasks",
+        "outputs": [
+          {
+            "name": "id",
+            "type": "uint256"
+          },
+          {
+            "name": "content",
+            "type": "string"
+          },
+          {
+            "name": "completed",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0x8d977672"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "taskCount",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0xb6cb58a5"
+      },
+      {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor",
+        "signature": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": false,
+            "name": "id",
+            "type": "uint256"
+          },
+          {
+            "indexed": false,
+            "name": "content",
+            "type": "string"
+          },
+          {
+            "indexed": false,
+            "name": "completed",
+            "type": "bool"
+          }
+        ],
+        "name": "TaskCreated",
+        "type": "event",
+        "signature": "0x05d0fb833127fc08168556d0e7ca9554fc3f6bc843b3b7d2bf1c35aea6bab660"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": false,
+            "name": "id",
+            "type": "uint256"
+          },
+          {
+            "indexed": false,
+            "name": "completed",
+            "type": "bool"
+          }
+        ],
+        "name": "TaskCompleted",
+        "type": "event",
+        "signature": "0xe21fa966ca5cd02748c0752352d18c48165e61cb55b4c29cccf924b5a95fcff1"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_content",
+            "type": "string"
+          }
+        ],
+        "name": "createTask",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function",
+        "signature": "0x111002aa"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_id",
+            "type": "uint256"
+          }
+        ],
+        "name": "toggleCompleted",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function",
+        "signature": "0x455f5024"
+      }
+    ]`);
+
+    const contract = new web3.eth.Contract(abi, address);
+    contract.methods
+      .taskCount()
+      .call()
+      .then((e) => {
+        console.log(e);
+      });
+    contract.methods
+      .tasks(1)
+      .call()
+      .then((e) => {
+        console.log(e);
+      });
   }
 }
 
